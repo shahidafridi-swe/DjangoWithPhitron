a9df3bb4b50ceb2e6323927238c3db01ab77db1c
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-05-23 00:47:38 +0600

    Module 10.5 - Django template filter

--- Module_10.5/django-template-filters-practice/first_app/views.py
+++ Module_10.5/django-template-filters-practice/first_app/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 
+from datetime import datetime
+
 
 
 def home(request):
     context = {
-        'name': "Shahid Afridi"
+        'name': "Shahid Afridi",
+        'age' : 27,
+        'bio' : "I'm a django developer",
+        'school' : "amnura high school",
+        'published' : datetime.now(),
+        'job': "",
+        'friends' : [
+            {'name': 'galib', 'age': 29},
+            {'name': 'nirob', 'age': 23},
+            {'name': 'kalin', 'age': 22},
+        ],
+        'hobby' : ['Travelling', 'Fishing', 'Gardening'],
+        'certificate' : """ PSC
+                            JSC
+                            SSC
+                            HSC
+                            BSC """,
     }
     return render(request, 'first_app/home.html', context)
 
