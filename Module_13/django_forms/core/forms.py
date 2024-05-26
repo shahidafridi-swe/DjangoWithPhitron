3a29b43d73c2b9c3e918191a4db015f70f2a91a3
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-05-26 21:41:03 +0600

    django form widget

--- Module_13/django_forms/core/forms.py
+++ Module_13/django_forms/core/forms.py
@@ -3,19 +3,26 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(label='Username',widget=forms.TextInput({ "placeholder": "Your Name", "class": "form-control"}))
     email = forms.EmailField(label='Your Email',widget=forms.TextInput({ "placeholder": "Your Email", "class": "form-control"}))
-    file = forms.FileField()
+    file = forms.FileField(required=False)
     
-    # age = forms.IntegerField()
-    # weight = forms.FloatField()
-    # balance = forms.DecimalField()
-    # married = forms.BooleanField(required=False)
+    about = forms.CharField(widget=forms.Textarea(attrs={'rows':3, 'placeholder': "Write about yourself..." , 'class': 'myClass'}))
+
+    age = forms.IntegerField(initial=27, disabled=True)
+    # age = forms.CharField(widget=forms.NumberInput())
+    weight = forms.FloatField()
+    balance = forms.DecimalField()
+    married = forms.BooleanField(required=False)
+
     # birthday = forms.DateField(widget = forms.SelectDateWidget)
-    # appoinment = forms.DateTimeField()
-    # CHOICES = [
-    #     ('S', 'Small'),
-    #     ('M', 'Medium'),
-    #     ('L', 'Large'),
-    # ]
-    # size = forms.ChoiceField(choices=CHOICES)
-    # MEAL = [('B', 'Biriyani'), ('C', 'Coffee'), ('T', 'Tea')]
-    # favouriteFood = forms.MultipleChoiceField(choices=MEAL)
+    # birthday = forms.DateField(widget = forms.DateInput(attrs={'type':'date'}))
+    birthday = forms.CharField(widget = forms.DateInput(attrs={'type':'date'}))
+
+    appoinment = forms.DateTimeField(widget = forms.DateInput(attrs={'type':'datetime-local'}))
+    CHOICES = [
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    ]
+    size = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    MEAL = [('B', 'Biriyani'), ('C', 'Coffee'), ('T', 'Tea')]
+    favouriteFood = forms.MultipleChoiceField(choices=MEAL, widget=forms.CheckboxSelectMultiple)
--- Module_13/django_forms/core/views.py
+++ Module_13/django_forms/core/views.py
@@ -25,10 +25,12 @@ def djangoForm(request):
     if request.method == 'POST':
         form = ContactForm(request.POST, request.FILES)
         if form.is_valid():
-            file = form.cleaned_data['file']
-            with open('./core/upload/' + file.name, 'wb+') as destination:
-                for chunk in file.chunks():
-                    destination.write(chunk)
+            
+            if form.cleaned_data['file']:
+                file = form.cleaned_data['file']
+                with open('./core/upload/' + file.name, 'wb+') as destination:
+                    for chunk in file.chunks():
+                        destination.write(chunk)
 
             print(form.cleaned_data)
             context['form'] = form
