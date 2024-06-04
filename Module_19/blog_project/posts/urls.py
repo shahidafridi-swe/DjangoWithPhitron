98f501f69b1c8306d825a9c354ee5b5cb0e9ffb5
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-06-05 01:43:38 +0600

    class based view - Post Create, Update and Delete

--- Module_19/blog_project/posts/urls.py
+++ Module_19/blog_project/posts/urls.py
@@ -4,8 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.posts, name='posts'),
     path('category/<slug:category_slug>/', views.posts, name='category_wise_posts'),
-    path('add_post/', views.addPost, name='add_post'),
-    path('update_post/<int:id>', views.updatePost, name='update_post'),
-    path('delete_post/<int:id>', views.deletePost, name='delete_post'),
+    # path('add_post/', views.addPost, name='add_post'),
+    path('add_post/', views.AddPost.as_view(), name='add_post'),
+    # path('update_post/<int:id>', views.updatePost, name='update_post'),
+    path('update_post/<int:id>', views.UpdatePost.as_view(), name='update_post'),
+    # path('delete_post/<int:id>', views.deletePost, name='delete_post'),
+    path('delete_post/<int:id>', views.DeletePost.as_view(), name='delete_post'),
     
 ]
--- Module_19/blog_project/posts/views.py
+++ Module_19/blog_project/posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
 from categories.models import Category
@@ -18,41 +21,85 @@ def posts(request, category_slug = None):
     return render(request, 'posts/posts.html', context)
 
 
-@login_required(login_url='login')
-def addPost(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            form.instance.author = request.user
-            form.save()
-            return redirect('posts')
-    else:
-        form = PostForm()
-    context = {'form' : form, 'type': 'Add'}
+# @login_required(login_url='login')
+# def addPost(request):
+#     if request.method == 'POST':
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             form.instance.author = request.user
+#             form.save()
+#             return redirect('posts')
+#     else:
+#         form = PostForm()
+#     context = {'form' : form, 'type': 'Add'}
     
-    return render(request, 'posts/add_post.html', context)
- 
-@login_required(login_url='login')
-def updatePost(request, id):
-    post = Post.objects.get(pk=id)
-    if(post.author != request.user):
-        return redirect('posts')
-    form = PostForm(instance=post)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.instance.author = request.user
-            form.save()
-            return redirect('posts')
-        
-    context = {'form' : form, 'type': 'Update'}
-    
-    return render(request, 'posts/add_post.html', context)
+#     return render(request, 'posts/add_post.html', context)
 
-@login_required(login_url='login')
-def deletePost(request, id):
-    post = Post.objects.get(pk=id)
-    if(post.author != request.user):
-        return redirect('posts')
-    post.delete()
-    return redirect('posts')
+
+class AddPost(LoginRequiredMixin,CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/add_post.html'
+    success_url = reverse_lazy('posts')
+    login_url = 'login'
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type'] = 'Add'
+        return context
+
+ 
+# @login_required(login_url='login')
+# def updatePost(request, id):
+#     post = Post.objects.get(pk=id)
+#     if(post.author != request.user):
+#         return redirect('posts')
+#     form = PostForm(instance=post)
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, instance=post)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('posts')
+#     context = {'form' : form, 'type': 'Update'}
+#     return render(request, 'posts/add_post.html', context)
+
+
+class UpdatePost(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/add_post.html'
+    success_url = reverse_lazy('posts')
+    login_url = 'login'
+    pk_url_kwarg = 'id'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type'] = 'Update'
+        return context
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+
+
+# @login_required(login_url='login')
+# def deletePost(request, id):
+#     post = Post.objects.get(pk=id)
+#     if(post.author != request.user):
+#         return redirect('posts')
+#     post.delete()
+#     return redirect('posts')
+
+class DeletePost(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    template_name = 'delete.html'
+    success_url = reverse_lazy('posts')
+    login_url = 'login'
+    pk_url_kwarg = 'id'
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+    
